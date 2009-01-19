f26a4c4d213db7e7dca08a3b9ca62b112aa3ad7d
kaistizen <kaistizen@588020a6-e61d-11dd-b2d9-215335d0b316>
2009-01-19 15:05:23 +0000

    2008년 10월 원고

---
+++ trunk/200810-Aiml/imedia,AimlTags/HangulUtil.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text;
+
+
+namespace imedia.AimlTags
+{
+    internal static class HangulUtil
+    {
+        // 초성, 중성, 종성에 대한 코드 테이블.
+        private static string m_ChoSungTbl = "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ";
+        private static string m_JungSungTbl = "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ";
+        private static string m_JongSungTbl = " ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ";
+
+        private static ushort m_UniCodeHangulBase = 0xAC00;
+        private static ushort m_UniCodeHangulLast = 0xD79F;
+
+        //자소 결합
+        public static string MergeJaso(string choSung, string jungSung, string jongSung)
+        {
+            int ChoSungPos, JungSungPos, JongSungPos;
+            int nUniCode;
+
+            ChoSungPos = m_ChoSungTbl.IndexOf(choSung);     // 초성 위치
+            JungSungPos = m_JungSungTbl.IndexOf(jungSung);   // 중성 위치
+            JongSungPos = m_JongSungTbl.IndexOf(jongSung);   // 종성 위치
+
+            // 앞서 만들어 낸 계산식
+            nUniCode = m_UniCodeHangulBase + (ChoSungPos * 21 + JungSungPos) * 28 + JongSungPos;
+
+            // 코드값을 문자로 변환
+            char temp = Convert.ToChar(nUniCode);
+            return temp.ToString();
+        }
+
+        //자소 분리(초성+중성+종성)
+        public static char[] DivideJaso(char hanChar)
+        {
+            int ChoSung, JungSung, JongSung;
+
+            ushort temp = 0x0000;
+
+            try
+            {
+                temp = Convert.ToUInt16(hanChar); //Char을 16비트 부호없는 정수형 형태로 변환
+            }
+            catch (Exception e)
+            {
+                //return e.ToString();
+            }
+
+            if ((temp < m_UniCodeHangulBase) || (temp > m_UniCodeHangulLast)) return null;
+
+            //초성자, 중성자, 종성자 Index계산
+            int nUniCode = temp - m_UniCodeHangulBase;
+            ChoSung = nUniCode / (21 * 28);
+            nUniCode = nUniCode % (21 * 28);
+            JungSung = nUniCode / 28;
+            nUniCode = nUniCode % 28;
+            JongSung = nUniCode;
+
+            return new char[] { m_ChoSungTbl[ChoSung], m_JungSungTbl[JungSung], m_JongSungTbl[JongSung] };
+        }
+
+        public enum LanguageType 
+        {
+            Unknown
+            , OverBound
+            , OneByteChar
+            , Korean // 완성형 문자
+            , KoreanJaum // 자음
+            , KoreanMoum // 모음
+            , Chinese // 한글 한자
+            , Japanese // 일본 가나
+        }
+
+        /// <summary>
+        /// KS-완성형 코드 기반 문자 판별 함수
+        /// </summary>
+        public static LanguageType GetLangageType(string sourceString)
+        {
+            if (sourceString.Length > 2)
+                return LanguageType.OverBound;
+
+            byte[] _tmp = Encoding.Default.GetBytes(sourceString);
+
+            if (_tmp.Length != 2)
+                return LanguageType.OneByteChar;
+
+            if ((_tmp[1] > 0xFE) || (_tmp[1] < 0xA1))
+                return LanguageType.Unknown;
+
+            if ((_tmp[0] >= 0xB0) && (_tmp[0] <= 0xC8)) // 완성형 문자
+                return LanguageType.Korean;
+            else if (_tmp[0] == 0xA4) // 한글 자모
+            {
+                if (_tmp[1] >= 161 && _tmp[1] <= 190) // 자음
+                    return LanguageType.KoreanJaum;
+                else // 모음
+                    return LanguageType.KoreanMoum;
+            }
+            else if ((_tmp[0] >= 0xCA) && (_tmp[0] <= 0xFD)) // 한글 한자
+                return LanguageType.Chinese;
+
+            if ((_tmp[0] >= 0xAA) && (_tmp[0] <= 0XAB)) // 일본 가나
+                return LanguageType.Japanese;
+
+            return LanguageType.Unknown;
+        }
+    }
+}
---
+++ trunk/200810-Aiml/imedia,AimlTags/josa.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+using AIMLbot.Utils; 
+
+namespace imedia.AimlTags
+{
+    [CustomTag]
+    public class josa : AIMLTagHandler
+    {
+        private readonly string _tagname = "josa";
+
+        public josa()
+        {
+            this.inputString = _tagname;
+        }
+
+        protected override string ProcessChange()
+        {
+            if( string.Compare(this.templateNode.Name, _tagname, true) != 0 )
+                return string.Empty;
+
+            if (this.templateNode.Attributes.Count != 1)
+                return string.Empty;
+
+            string subject = this.templateNode.InnerText;
+            string suffix = string.Empty;
+
+            if (subject.Length == 0)
+                return string.Empty;
+
+            foreach(XmlAttribute xmlAttr in this.templateNode.Attributes)
+            {
+
+                if(xmlAttr.Name == "suffix")
+                    suffix = xmlAttr.Value;
+            }
+
+            return Combine(subject, suffix);
+        }
+
+        internal static string Combine(string subject, string suffix)
+        {
+            if (suffix != "은는" && suffix != "이가" && suffix != "과와" && suffix != "을를")
+            {
+                if (subject.Length > 0)
+                {
+                    return subject;
+                }
+                return string.Empty;
+            }
+
+            char[] josaCandidates = suffix.ToCharArray();
+
+
+            return Combine(subject, josaCandidates);
+        }
+
+        private static string Combine(string subject, char[] josaCadidates)
+        {            
+            char lastChar = subject[subject.Length - 1];
+            string lastCharString = lastChar.ToString();
+
+            HangulUtil.LanguageType lang = HangulUtil.GetLangageType(lastCharString);
+            if(
+                lang != HangulUtil.LanguageType.Korean
+                && lang != HangulUtil.LanguageType.KoreanJaum
+                && lang != HangulUtil.LanguageType.KoreanMoum
+                ) // 한국어가 아닐 때
+            {
+                RegexOptions options = RegexOptions.IgnorePatternWhitespace;
+                options |= RegexOptions.IgnoreCase;
+
+                Regex regex = new Regex("[aeiou]", options);
+                if (regex.IsMatch(lastCharString) == true)
+                    return subject + josaCadidates[1];
+
+                return subject + josaCadidates[0];
+            }
+            
+            // 한국어일 때
+            char[] decomposed = HangulUtil.DivideJaso(lastChar);
+
+            if ( 
+                decomposed.Length < 3
+                || (decomposed.Length == 3 && decomposed[2] == ' ')
+                )
+            {
+                return subject+josaCadidates[1];
+            }
+
+            return subject+josaCadidates[0];
+        }
+    }
+}
