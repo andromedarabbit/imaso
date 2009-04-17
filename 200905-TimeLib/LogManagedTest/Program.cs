720d7f109d18d121e2545fe18c4a861b47d3f0ea
kaistizen <kaistizen@588020a6-e61d-11dd-b2d9-215335d0b316>
2009-04-17 09:52:20 +0000

    기존 단위테스트 프로젝트를 다시 추가한다. 아직은 빌드가 깨지는 상황

---
+++ 200905-TimeLib/LogManagedTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LogManagedTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogManaged.Write("does it work?");
+        }
+    }
+}
