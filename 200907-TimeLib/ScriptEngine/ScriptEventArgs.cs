150367c738df22ffdd2c535f81f3e338f5e29578
kaistizen <kaistizen@588020a6-e61d-11dd-b2d9-215335d0b316>
2009-07-19 11:20:35 +0000

    빼먹은 파일

---
+++ 200907-TimeLib/ScriptEngine/ScriptEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ScriptEngine
+{
+    public abstract class ScriptEventArgs
+    {
+        private readonly int _eventNo;
+
+        protected ScriptEventArgs(int eventNo)
+        {
+            this._eventNo = eventNo;
+        }
+
+        public int EventNo
+        {
+            get { return _eventNo; }
+        }
+    }
+}
