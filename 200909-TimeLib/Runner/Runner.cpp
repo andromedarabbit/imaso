5ec272514b57f4f796a896f1eb08a7ef42bd2f41
kaistizen <kaistizen@588020a6-e61d-11dd-b2d9-215335d0b316>
2009-08-17 13:34:40 +0000

    9월 원고

---
+++ 200909-TimeLib/Runner/Runner.cpp
@@ -0,0 +1,12 @@
+// Runner.cpp : 기본 프로젝트 파일입니다.
+
+#include "stdafx.h"
+#include "EntryPoint.h"
+
+using namespace System;
+
+int main(array<System::String ^> ^args)
+{
+	EntryPoint();
+    return 0;
+}
